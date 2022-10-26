758194618a4eebe333b2dd1655425f08e6981c6b
julien-c75 <juliencartigny01@gmail.com>
2022-10-26 18:54:59 +0200

    fini ?

--- 64-Cartigny-Ung/dpll.ml
+++ 64-Cartigny-Ung/dpll.ml
@@ -14,7 +14,7 @@ let filter_map filter list =
     | h::t -> match (filter h) with
       | None   -> aux t ret
       | Some e -> aux t (e::ret)
-  in aux list []
+  in aux list [];;
 
 (* print_modele : int list option -> unit
    affichage du résultat *)
@@ -23,15 +23,15 @@ let print_modele: int list option -> unit = function
   | Some modele -> print_string "SAT\n";
      let modele2 = sort (fun i j -> (abs i) - (abs j)) modele in
      List.iter (fun i -> print_int i; print_string " ") modele2;
-     print_string "0\n"
+     print_string "0\n";;
 
 (* ensembles de clauses de test *)
-let exemple_3_12 = [[1;2;-3];[2;3];[-1;-2;3];[-1;-3];[1;-2]]
-let exemple_7_2 = [[1;-1;-3];[-2;3];[-2]]
-let exemple_7_4 = [[1;2;3];[-1;2;3];[3];[1;-2;-3];[-1;-2;-3];[-3]]
-let exemple_7_8 = [[1;-2;3];[1;-3];[2;3];[1;-2]]
-let systeme = [[-1;2];[1;-2];[1;-3];[1;2;3];[-1;-2]]
-let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20;21];[-1;-2];[-1;-3];[-2;-3];[-4;-5];[-4;-6];[-5;-6];[-7;-8];[-7;-9];[-8;-9];[-10;-11];[-10;-12];[-11;-12];[-13;-14];[-13;-15];[-14;-15];[-16;-17];[-16;-18];[-17;-18];[-19;-20];[-19;-21];[-20;-21];[-1;-4];[-2;-5];[-3;-6];[-1;-7];[-2;-8];[-3;-9];[-4;-7];[-5;-8];[-6;-9];[-4;-10];[-5;-11];[-6;-12];[-7;-10];[-8;-11];[-9;-12];[-7;-13];[-8;-14];[-9;-15];[-7;-16];[-8;-17];[-9;-18];[-10;-13];[-11;-14];[-12;-15];[-13;-16];[-14;-17];[-15;-18]]
+let exemple_3_12 = [[1;2;-3];[2;3];[-1;-2;3];[-1;-3];[1;-2]];;
+let exemple_7_2 = [[1;-1;-3];[-2;3];[-2]];;
+let exemple_7_4 = [[1;2;3];[-1;2;3];[3];[1;-2;-3];[-1;-2;-3];[-3]];;
+let exemple_7_8 = [[1;-2;3];[1;-3];[2;3];[1;-2]];;
+let systeme = [[-1;2];[1;-2];[1;-3];[1;2;3];[-1;-2]];;
+let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20;21];[-1;-2];[-1;-3];[-2;-3];[-4;-5];[-4;-6];[-5;-6];[-7;-8];[-7;-9];[-8;-9];[-10;-11];[-10;-12];[-11;-12];[-13;-14];[-13;-15];[-14;-15];[-16;-17];[-16;-18];[-17;-18];[-19;-20];[-19;-21];[-20;-21];[-1;-4];[-2;-5];[-3;-6];[-1;-7];[-2;-8];[-3;-9];[-4;-7];[-5;-8];[-6;-9];[-4;-10];[-5;-11];[-6;-12];[-7;-10];[-8;-11];[-9;-12];[-7;-13];[-8;-14];[-9;-15];[-7;-16];[-8;-17];[-9;-18];[-10;-13];[-11;-14];[-12;-15];[-13;-16];[-14;-17];[-15;-18]];;
 
 (********************************************************************)
 
@@ -39,6 +39,7 @@ let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20
    applique la simplification de l'ensemble des clauses en mettant
    le littéral l à vrai *)
 exception Vide
+
 
 let rec simplifie l clauses =
   (*Pour chaques closes*)
@@ -99,7 +100,7 @@ let rec solveur_split clauses interpretation =
   let branche = solveur_split (simplifie l clauses) (l::interpretation) in
   match branche with
   | None -> solveur_split (simplifie (-l) clauses) ((-l)::interpretation)
-  | _    -> branche
+  | _    -> branche;;
 
 (* tests *)
 (* let () = print_modele (solveur_split systeme []) *)
@@ -244,10 +245,22 @@ let pur clauses =
    pur [[1;4;-5];[4;-7;-2;-5];[7;-1;2;5;-4]];;
    pur [[1;4;-5];[4;-7;-2;-5];[7;-1;2;5;4]];;*)
 
+   
 (* solveur_dpll_rec : int list list -> int list -> int list option *)
 let rec solveur_dpll_rec clauses interpretation =
-  (* à compléter *)
-  None
+  if clauses = [] then
+    Some interpretation
+  else if List.mem [] clauses then
+    None
+  else let l = try (pur clauses, true) with 
+  | Failure_no_pur -> (hd (hd clauses), false)
+  in match l with
+  | (l, true) -> solveur_dpll_rec (simplifie l clauses) (l::interpretation)
+  | (l, false) -> 
+    let branche = solveur_dpll_rec (simplifie l clauses) (l::interpretation) in
+    match branche with
+    | None -> solveur_dpll_rec (simplifie (-l) clauses) ((-l)::interpretation)
+    | _    -> branche;;
 
 (* tests *)
 (* let () = print_modele (solveur_dpll_rec systeme []) *)
@@ -255,4 +268,4 @@ let rec solveur_dpll_rec clauses interpretation =
 
 let () =
   let clauses = Dimacs.parse Sys.argv.(1) in
-  print_modele (solveur_dpll_rec clauses [])
+  print_modele (solveur_dpll_rec clauses []);;
